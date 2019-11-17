8243643aed7ed7d3cf5b6ca879ce25229c30bdb1
Ivan <fivanunam@gmail.com>
2019-11-17 12:18:21 -0600

    book titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,30 @@
 class Book
-# write your code here
+    def initialize(title = "No Title")
+        @title = title
+    end
+    
+    def title
+        @title
+    end
+    
+    def title=(title)
+        noCapitalize  = [
+            "the", "a", "an",                                       #articles
+            "for", "and", "nor", "but", "or", "yet", "so", "of",    #conjunctions
+            "in", "on", "after", "before"                           #propositions
+            ]
+
+        @title = (title.split(" ").collect { |word|
+            noCapitalize.include?(word) ?
+            word :
+            word.capitalize 
+        }).join(" ")
+
+        @title = @title[0...1].capitalize  + @title[1, @title.length]
+    end
 end
+
+
+book = Book.new
+book.title = "a title"
+puts book.title
