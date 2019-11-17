52e01d5328dde1b486687b8aeb656de25a3c7869
Ivan <fivanunam@gmail.com>
2019-11-17 13:37:09 -0600

    timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,27 @@
 class Timer
-  #write your code here
+  def initialize seconds = 0
+    @seconds = seconds
+  end
+
+  def seconds
+    @seconds
+  end
+  def seconds=(seconds)
+    @seconds = seconds
+  end
+
+  def padded seconds
+    "%02d" % seconds
+  end
+
+  def time_string
+    hours = padded @seconds.divmod(3600)
+    minutes = padded (@seconds % 3600).divmod(60)
+    seconds = padded ((@seconds % 3600) % 60)
+
+    "#{hours}:#{minutes}:#{seconds}"
+  end
 end
+
+timer = Timer.new 4000
+puts timer.time_string
