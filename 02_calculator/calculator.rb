714a372762e98434972b8832f1671d2abfdfb660
Ivan <fivanunam@gmail.com>
2019-11-16 20:25:24 -0600

    calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,28 @@
 #write your code here
+def add num1, num2
+    num1 + num2
+end
+
+def subtract num1, num2
+    num1 - num2
+end
+
+def sum arr
+    arr.sum
+end
+
+def multiply *args
+    args.reduce(1, :*)
+end
+
+def power base, pow
+    base ** pow
+end
+
+def factorial num
+    num == 0 ? 1 : factorial(num - 1) * num
+end
+
+
+
+
